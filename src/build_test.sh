fc2c0a9c62fb571f912976184fc28644a84f4451
Marbax <marbax117@gmail.com>
2019-04-29 09:27:00 +0300

    compose and gitlub runner created

--- src/build_test.sh
+++ src/build_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #export USER_NAME=marbax
-for i in ui crawler; do cd $i; docker build . -t marbax/$i:test; cd -; done
+for i in crawler-ui crawler-app; do cd $i; docker build . -t marbax/$i:test; cd -; done
