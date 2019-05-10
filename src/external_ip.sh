4972ab9c65291766cb65f2e6b2c2a97c4f6affa3
Marbax <marbax117@gmail.com>
2019-05-10 16:22:30 +0300

    Looks like all working

--- src/external_ip.sh
+++ src/external_ip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sed -i -e "s/ip_of_external_machine/$(curl 2ip.ru)/g" /home/appuser/.env
 cd /home/appuser/
-docker-compose -f docker-compose-gitlab.yml restart
+docker-compose -f docker-compose-gitlab.yml stop
+docker-compose -f docker-compose-gitlab.yml up -d
+
